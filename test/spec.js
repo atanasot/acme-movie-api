84a9a7939a264aa1ab0c6cc3dc34916603e53f51
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-12 16:40:41 -0400

    first commit

---
+++ test/spec.js
@@ -0,0 +1,19 @@
+const {expect} = require('chai')
+
+describe('the sky', () => {
+    describe('the sky is blue', ()=> {
+        it('the sky is always blue', ()=> {
+            console.log('the sky is green')
+        })
+    })
+})
+
+// describe('Routes', ()=> {
+//     describe('GET /', ()=> {
+//         it('this is the ACME Movie API page', async()=> {
+//             const response = await app.get('/')
+//             expect(response.status).to.equal(200)
+//             expect(response.text).to.include('ACME Movie API')
+//         })
+//     })
+// })
