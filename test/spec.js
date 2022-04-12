b56a2050450bfad2179bac8ba662aa34d5ee3d5a
Lina Ryerkerk <lina.ryerkerk@gmail.com>
2022-04-12 17:10:05 -0400

    added data layer

--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,5 @@
 const {expect} = require('chai')
+const {syncAndSeed} = require('../db')
 
 const app = require('supertest')(require('../app')) // what is supertest -- it tests express API
 
@@ -11,6 +12,7 @@ describe('the sky', () => {
 })
 
 describe('Routes', ()=> {
+    before(() => syncAndSeed()) // no await here, we dont want a promise
     describe('GET /', ()=> {
         it('this is the ACME Movie API page', async()=> {
             const response = await app.get('/')
